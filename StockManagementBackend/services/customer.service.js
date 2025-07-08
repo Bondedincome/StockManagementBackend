490e68d9b080c83b18b2012844aa6b44de8e14ce
BondedIncome <daniotmw10@gmail.com>
2025-07-08 09:43:35 +0300

    fix: After adding the column `isSupplier` I had to make a condition to not show those that are Suppliers.

--- StockManagementBackend/services/customer.service.js
+++ StockManagementBackend/services/customer.service.js
@@ -1,52 +1,52 @@
 const prisma = require("../prisma/client");
 
 const getAllCustomersService = async () => {
-    return await prisma.customer.findMany({
-        where: { isDeleted: false },
-        include: { user: true },
-    });
+	return await prisma.customer.findMany({
+		where: { isDeleted: false, isSupplier: false },
+		include: { user: true },
+	});
 };
 
 const getOneCustomerService = async (id) => {
-    return await prisma.customer.findUnique({
-        where: { customerId: id },
-        include: { user: true },
-    });
+	return await prisma.customer.findUnique({
+		where: { customerId: id, isDeleted: false, isSupplier: false },
+		include: { user: true },
+	});
 };
 
 const createCustomerService = async (data) => {
-    return await prisma.customer.create({ data });
+	return await prisma.customer.create({ data });
 };
 
 const updateCustomerService = async (id, data) => {
-    return await prisma.customer.update({
-        where: { customerId: id },
-        data,
-    });
+	return await prisma.customer.update({
+		where: { customerId: id, isDeleted: false, isSupplier: false },
+		data,
+	});
 };
 
 const deleteCustomerService = async (id) => {
-    return await prisma.customer.update({
-        where: { customerId: id },
-        data: { isDeleted: true, deletedAt: new Date() },
-    });
+	return await prisma.customer.update({
+		where: { customerId: id, isSupplier: false },
+		data: { isDeleted: true, deletedAt: new Date() },
+	});
 };
 
 const getPaginatedCustomersService = async (page, limit) => {
-    const skip = (page - 1) * limit;
-    return await prisma.customer.findMany({
-        where: { isDeleted: false },
-        skip,
-        take: limit,
-        include: { user: true },
-    });
+	const skip = (page - 1) * limit;
+	return await prisma.customer.findMany({
+		where: { isDeleted: false, isSupplier: false },
+		skip,
+		take: limit,
+		include: { user: true },
+	});
 };
 
 module.exports = {
-    getAllCustomersService,
-    getOneCustomerService,
-    createCustomerService,
-    updateCustomerService,
-    deleteCustomerService,
-    getPaginatedCustomersService,
+	getAllCustomersService,
+	getOneCustomerService,
+	createCustomerService,
+	updateCustomerService,
+	deleteCustomerService,
+	getPaginatedCustomersService,
 };
